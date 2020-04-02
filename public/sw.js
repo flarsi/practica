6762074ccc6c30d4a119e0a321ebdbccbd0dbb73
Danil <danil.zap63.dd@gmail.com>
2020-04-02 14:44:04 +0300

    5 day

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open("static")
+            .then(function (cache) {
+                console.log("precaching")
+                cache.add("/")
+                cache.add("/index.html")
+                cache.add("/images/logo.png")
+                cache.add("/css/bootstrap.min.css")
+            })
+    )
+});
+self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating Service Worker ...', event);
+    return self.clients.claim();
+
+});
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response){
+                    return response
+                }else fetch(event.request)
+            })
+    )
+});
